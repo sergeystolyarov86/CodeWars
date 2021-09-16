939d2a4867db3ce799940f070e08cbe663272afe
sergeystolyarov86 <sergey_stolyarov@mail.ru>
2021-09-17 00:08:31 +0300

    Digital_rootTask

--- src/Stolyarov_S/Digital_root.java
+++ src/Stolyarov_S/Digital_root.java
@@ -6,9 +6,9 @@ package Stolyarov_S;
 //     942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 //    132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 
-public class Main {
+public class Digital_root {
     public static void main(String[] args) {
-        System.out.println(Main.digital_root(12345));
+        System.out.println(Digital_root.digital_root(12345));
 
     }
 
